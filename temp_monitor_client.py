eac36092721c4aba55afef8aad8d2235adb46030
Mariana F <marianafama@unisabana.edu.co>
2026-04-14 14:42:00 -0500

    12

--- temp_monitor_client.py
+++ temp_monitor_client.py
@@ -5,24 +5,6 @@
 import temp_monitor
 
 
-def main():
-
-    archivo = input("Nombre del archivo")
-
-    archivo = open(archivo, "r")
-    n = int(archivo.realice)
-
-    monitor = temp_monitor.init(n)
-
-    for i in range(n):
-        temp = float(archivo.realice{})
-        monitor = temp_monitor.add_realice(monitor, temp)
-
-    archivo.close()
-    print()
-
-pass
-
 
 if __name__ == "__main__":
     main()
